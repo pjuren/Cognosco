41afe89345898fe5a901c1b0b88b7b1898039689
pjuren <philip.uren@gmail.com>
2015-02-28 20:31:47 -0800

    adding hpp for clustering common defs etc., and fixing refs to stl types in KMedoids.hpp

--- src/clustering/KMedoids.hpp
+++ src/clustering/KMedoids.hpp
@@ -17,22 +17,31 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#ifndef KMEDOIDS_HPP_
+#define KMEDOIDS_HPP_
+
+// stl includes
+#include <set>
+#include <string>
+#include <vector>
+
+// Cognosco includes
+#include "clustering.hpp"
 
 class KMedoidsClusterer {
 public:
   // constructors
   KMedoidsClusterer(const size_t k, const DistanceMatrix &dist_m,
-                    const vector<string> &instance_ids);
+                    const std::vector<std::string> &instance_ids);
 
   // public inspectors
-  const set<string>&  get_medoids() const;
-  set<string> get_medoid_members(const string medoid) const;
-  string get_cluster_assignment(const string &instance_name) const;
-  double compute_membership_probability(const string &instance,
-                                 const string &medoid) const;
-  string find_closest_medoid(const string &instance_name);
-  double get_distance(const string &s, const string &t);
-
+  const std::set<std::string>&  get_medoids() const;
+  std::set<std::string> get_medoid_members(const std::string medoid) const;
+  std::string get_cluster_assignment(const std::string &instance_name) const;
+  double compute_membership_probability(const std::string &instance,
+                                        const std::string &medoid) const;
+  std::string find_closest_medoid(const std::string &instance_name);
+  double get_distance(const std::string &s, const std::string &t);
 
   // public mutators
   void train();
@@ -40,16 +49,18 @@ public:
 private:
   // private inspectors
   double cost();
-  bool is_medoid(const string &id) const;
+  bool is_medoid(const std::string &id) const;
 
   // private mutators
-  void swap_medoid(const string &non_medoid, const string &medoid);
+  void swap_medoid(const std::string &non_medoid, const std::string &medoid);
   void update_cluster_assignments();
-  void assign_to_medoid(const string &instance, const string medoid);
+  void assign_to_medoid(const std::string &instance, const std::string medoid);
 
   // private instance variables
-  set<string> medoids;
+  std::set<std::string> medoids;
   DistanceMatrix distance_matrix;
   vector<string> instance_ids;
   unordered_map<string, string> cluster_assignments;
 };
+
+#endif
